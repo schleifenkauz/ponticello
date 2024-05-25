b122947ce6180dd3354bb79edccc9dc83363254f
NKB03 <niko.knop003@gmail.com>
2024-05-25 13:46:47 +0200

    add SuperCollider logic to CompoundScoreObject

--- src/main/kotlin/xenakis/model/CompoundScoreObject.kt
+++ src/main/kotlin/xenakis/model/CompoundScoreObject.kt
@@ -1,8 +1,10 @@
 package xenakis.model
 
+import hextant.context.Context
 import hextant.core.editor.ViewManager
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.JsonObjectBuilder
+import xenakis.impl.ScWriter
 import xenakis.impl.getSerializableValue
 import xenakis.impl.putSerializableValue
 import xenakis.ui.ScoreObjectView
@@ -12,6 +14,21 @@ class CompoundScoreObject(name: String, val score: Score) : AbstractScoreObject(
         get() = "compound"
 
     override val viewManager = ViewManager.createWeakViewManager<ScoreObjectView>()
+
+    override fun addToScore(score: Score, context: Context) {
+        super.addToScore(score, context)
+        for (obj in score.objects) {
+            obj.addToScore(score, context)
+        }
+    }
+
+    override fun writeStartCode(writer: ScWriter, offset: Double) {
+        score.writePlayerTask(writer, offset, taskName = "play_$name")
+    }
+
+    override fun writeStopCode(writer: ScWriter) {
+        writer.appendLine("play_$name.stop;")
+    }
 
     override fun copy(): ScoreObject = CompoundScoreObject(name, score)
 
