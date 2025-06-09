eb74a0ceaf5d7133777dafd23dbbcf51a72592ab
Nikolaus Knop <niko.knop003@gmail.com>
2025-06-09 16:09:38 +0200

    Make the interaction config popup a bit more intuitive

--- src/main/kotlin/ponticello/model/project/InlineControlsDisplay.kt
+++ src/main/kotlin/ponticello/model/project/InlineControlsDisplay.kt
@@ -4,5 +4,12 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 enum class InlineControlsDisplay {
-    NONE, MINIMAL_OVERLAY, EXTENDED_OVERLAY, CONTROLS_BAR
+    NONE, MINIMAL_OVERLAY, EXTENDED_OVERLAY, CONTROLS_BAR;
+
+    override fun toString(): String = when (this) {
+        NONE -> "None"
+        MINIMAL_OVERLAY -> "Minimal Overlay"
+        EXTENDED_OVERLAY -> "Extended Overlay"
+        CONTROLS_BAR -> "Controls Bar"
+    }
 }
--- src/main/kotlin/ponticello/ui/misc/InteractionConfigBar.kt
+++ src/main/kotlin/ponticello/ui/misc/InteractionConfigBar.kt
@@ -1,19 +1,23 @@
 package ponticello.ui.misc
 
-import fxutils.KeyEventHandlerBody
+import fxutils.*
 import fxutils.actions.action
 import fxutils.actions.makeButton
 import fxutils.actions.registerActions
-import fxutils.infiniteSpace
+import fxutils.prompt.DetailPane
 import fxutils.prompt.SimpleSearchableListView
-import fxutils.styleClass
+import fxutils.undo.UndoManager
+import javafx.scene.control.CheckBox
 import javafx.scene.control.Label
 import javafx.scene.layout.HBox
 import javafx.scene.layout.Region
 import org.kordamp.ikonli.materialdesign2.MaterialDesignD
 import org.kordamp.ikonli.materialdesign2.MaterialDesignM
+import ponticello.model.project.InlineControlsDisplay
 import ponticello.model.project.UIState
 import ponticello.model.score.TimeUnit
+import ponticello.ui.impl.DEFAULT_SCENE_FILL
+import ponticello.ui.impl.sceneFill
 import reaktive.value.binding.map
 import reaktive.value.binding.not
 import reaktive.value.fx.asObservableValue
@@ -52,7 +56,7 @@ class InteractionConfigBar(private val settings: UIState) : HBox() {
     fun addGridRelatedShortcuts(body: KeyEventHandlerBody<Unit>) {
         for (option in TimeUnit.entries) {
             val shortcut = shortcutFor(option)
-            body.on(shortcut){
+            body.on(shortcut) {
                 settings.snapOption.now = option
             }
         }
@@ -61,6 +65,30 @@ class InteractionConfigBar(private val settings: UIState) : HBox() {
 
 
     companion object {
+        private fun createDetailsPane(config: UIState): DetailPane = DetailPane(labelWidth = 160.0).apply {
+            addItem(
+                "Ask for group names", CheckBox().sync(
+                    config.askForGroupNames,
+                    "Ask for group names", config.context[UndoManager]
+                )
+            )
+            addItem(
+                "Ask for clone names", CheckBox().sync(
+                    config.askForCloneNames,
+                    "Ask for clone names", config.context[UndoManager]
+                )
+            )
+            addItem(
+                "Inline display mode",
+                SimpleSearchableListView(InlineControlsDisplay.entries, "Select controls display mode")
+                    .selectorButton(
+                        config.controlsDisplay,
+                        undoManager = config.context[UndoManager],
+                        actionDescription = "Select inline display mode"
+                    ).setFixedWidth(140.0)
+            )
+        }
+
         private val toggleSnap = action<UIState>("Toggle snapping") {
             shortcut("Q")
             icon { settings ->
@@ -72,11 +100,13 @@ class InteractionConfigBar(private val settings: UIState) : HBox() {
             toggles(UIState::snapEnabled)
         }
 
-        private val showExtraSettings = action<UIState>("Extra settings") {
+        private val showExtraSettings = action("Extra settings") {
             icon(MaterialDesignD.DOTS_VERTICAL)
             executes { settings, ev ->
-                val dialog = InteractionConfigDialog(settings)
-                dialog.showDialog(ev)
+                val detailsPane = createDetailsPane(settings)
+                val popup = SubWindow(detailsPane, "Interaction configuration", SubWindow.Type.Popup)
+                    .sceneFill(DEFAULT_SCENE_FILL.opacity(0.5))
+                popup.show(ev.popupAnchor())
             }
         }
     }
